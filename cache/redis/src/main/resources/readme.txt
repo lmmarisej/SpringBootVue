66b4e80e2fc0912ad03e01de61f6bd9ce9e914d8
lmmarisej <lmmarise@foxmail.com>
2021-10-17 14:13:04 +0800

    fix:redis集群脚本

--- cache/redis/src/main/resources/readme.txt
+++ cache/redis/src/main/resources/readme.txt
@@ -1,1 +1,17 @@
-推荐open finder执行脚本。在idea执行，idea会有bug。
+1. 推荐切换到脚本所在目录，再执行。
+2. 推荐open finder执行脚本。在idea执行，idea会有bug (工作目录问题)。
+
+./redis-cli --cluster create 127.0.0.1:7001 127.0.0.1:7002 127.0.0.1:7003 127.0.0.1:7004 127.0.0.1:7005 127.0.0.1:7006 127.0.0.1:7007 127.0.0.1:7008 127.0.0.1:7009 --cluster-replicas 1
+
+
+bind 192.168.120.4
+port 7000
+daemonize yes
+appendonly yes
+protected-mode no
+cluster-enabled yes
+cluster-config-file nodes.conf
+cluster-node-timeout 15000
+pidfile "/opt/redis-cluter/7000/pidfile/redis.pid"
+logfile "/opt/redis-cluter/7000/logfile/redis.log"
+dir "/opt/redis-cluter/7000/data"
--- cache/redis/src/main/resources/redis-cluster-run.sh
+++ cache/redis/src/main/resources/redis-cluster-run.sh
@@ -1,13 +1,14 @@
 echo "==============================当前工作目录==============================="
-workdir=$(cd $(dirname $0); pwd)
+workdir="$(cd $(dirname $0); pwd)/redis-cluster"
 echo "$workdir"
+# 层级文件创建
+mkdir -p "$workdir"
+cd "$workdir"
 echo "=============================创建节点配置文件=============================="
 for i in {1..9}; do
-  conf_file="$workdir/redis-cluster/700${i}.conf"
+  conf_file="$workdir/700${i}.conf"
   conf_content="port 700${i}\r\ndaemonize no\r\ncluster-enabled yes\r\ncluster-config-file cluster-nodes-700${i}.conf\r\ncluster-node-timeout 1000\r\ncluster-replica-validity-factor 5\r\ncluster-migration-barrier 1\r\ncluster-require-full-coverage yes\r\ncluster-replica-no-failover no\r\nappendonly yes"
   echo "创建文件并写入配置数据：$conf_file"
-  # 层级文件创建
-  mkdir -p "$workdir/redis-cluster"
   # 创建配置文件
   touch "${conf_file}"
   # 避免因为权限而找不到文件
@@ -18,7 +19,7 @@ done
 echo "==============================节点启动信息==============================="
 conf_list=""
 for i in {1..9}; do
-  redis-server "$workdir"/redis-cluster/700"${i}".conf &
+  redis-server "$workdir"/700"${i}".conf &
   conf_list="$conf_list""[redis-cluster/700${i}.conf]"
 done
 echo "redis-server run with cluster node:{$conf_list}"
@@ -28,6 +29,7 @@ redis-cli -c -h 127.0.0.1 -p 7001 cluster nodes
 echo "===============================集群信息=================================="
 redis-cli -c -h 127.0.0.1 -p 7001 cluster info
 echo "==============================集群槽位信息================================="
+redis-cli --cluster create 127.0.0.1:7001 127.0.0.1:7002 127.0.0.1:7003 127.0.0.1:7004 127.0.0.1:7005 127.0.0.1:7006 127.0.0.1:7007 127.0.0.1:7008 127.0.0.1:7009 --cluster-replicas 1
 redis-cli -c -h 127.0.0.1 -p 7001 cluster slots
 echo "==============================集群进程信息================================="
 ps aux|grep redis
