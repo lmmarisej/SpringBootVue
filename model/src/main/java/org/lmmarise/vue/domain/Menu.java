8bb7155555d389d1e75daa09be52d74a8d86f5d3
lmmarisej <lmmarise@foxmail.com>
2021-10-19 22:23:57 +0800

    fix:修正Menu子Menu树形菜单

--- model/src/main/java/org/lmmarise/vue/domain/Menu.java
+++ model/src/main/java/org/lmmarise/vue/domain/Menu.java
@@ -4,8 +4,8 @@ import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.*;
 
 import javax.persistence.*;
-import java.util.ArrayList;
 import java.util.Collection;
+import java.util.HashSet;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -30,31 +30,9 @@ public class Menu extends AuditModel {
 
     @JsonIgnore
     @ManyToMany(mappedBy = "menus", cascade = CascadeType.MERGE, fetch = FetchType.LAZY)
-    private Collection<Role> roles = new ArrayList<>();       // 中间表由 Role 来维护
+    private Collection<Role> roles = new HashSet<>();       // 中间表由 Role 来维护
 
-    @JsonIgnore
-    @OneToOne
-    @JoinColumn(name = "parentMenuId")
-    private Menu parentMenu;
-
-    @JsonIgnore
-    @OneToMany(cascade = {CascadeType.PERSIST, CascadeType.REMOVE}, fetch = FetchType.EAGER)
-    @JoinColumn(name = "id")
-    private Collection<Menu> childrenMenu = new ArrayList<>();
-
-    @Transient
-    private Integer parentMenuId;
-
-    @Transient
-    private Collection<Integer> childrenMenuIds;
-
-    public Integer getParentMenuId() {
-        return parentMenu != null ? parentMenu.getId() : null;
-    }
-
-    public List<Integer> getChildrenMenuIds() {
-        return childrenMenu != null
-                ? childrenMenu.stream().map(AuditModel::getId).collect(Collectors.toList())
-                : null;
-    }
+    @OneToMany(cascade = CascadeType.ALL, fetch = FetchType.EAGER)
+    @JoinTable(name = "menuChildren")
+    private Collection<Menu> childrenMenu = new HashSet<>();
 }
